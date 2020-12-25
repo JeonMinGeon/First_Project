f5dc784ed285adf4e351e5bedb7daff6e44d10c2
Jeon Min Geon <wjsxorjs@github.com>
2020-12-25 12:59:35 +0900

    evaluation and adjusting threshold with modifyied calculation

--- testHW_diff_1.py
+++ testHW_diff_1.py
@@ -36,7 +36,7 @@ def main():
 
     frame_gray_avg = frame_gray_sum / (len(input))
     frame_gray_avg.astype(np.uint8)
-    cv.imshow('Average', frame_gray_avg)
+    # cv.imshow('Average', frame_gray_avg)
     cv.imwrite(os.path.join(basic_path, 'average.png'), frame_gray_avg)
 
     
@@ -52,7 +52,7 @@ def main():
         avg_diff_abs = np.abs(avg_diff).astype(np.float64)
 
         ##### make mask by applying threshold
-        avg_frame_diff_max_thres = np.where(avg_diff_abs > 70, 0.5, -0.5)
+        avg_frame_diff_max_thres = np.where(avg_diff_abs > 55, 0.5, -0.5)
         avg_frame_diff_min_thres = np.where(avg_diff_abs > 5, 0.5, -0.5)
         avg_frame_diff = avg_frame_diff_max_thres + avg_frame_diff_min_thres
 
@@ -62,7 +62,7 @@ def main():
 
         ##### final result
         avg_result = avg_current_gray_masked_mk2.astype(np.uint8)
-        cv.imshow('avg_result', avg_result)
+        # cv.imshow('avg_result', avg_result)
 
         ##### make result file
         ##### Please don't modify path
@@ -72,21 +72,36 @@ def main():
         if image_idx == len(input) - 1:
             break
 
-    
+    eval.cal_result(gt_path, avg_path)
 
+    #*********************************************************************#
+    
     frame_current = cv.imread(os.path.join(input_path, input[0]))
+    frame_current_gray = cv.cvtColor(frame_current, cv.COLOR_BGR2GRAY).astype(np.float64)
+    frame_next = cv.imread(os.path.join(input_path, input[2]))
+    frame_next_gray = cv.cvtColor(frame_current, cv.COLOR_BGR2GRAY).astype(np.float64)
+    frame_prev_gray = frame_current_gray
+    frame_current = cv.imread(os.path.join(input_path, input[1]))
     frame_current_gray = cv.cvtColor(frame_current, cv.COLOR_BGR2GRAY).astype(np.float64)
 
     ##### background substraction
-    for image_idx in range(len(input)):
-
+    for image_idx in range(1,len(input)):
 
         ##### calculate foreground region
-        diff = frame_current_gray - frame_prev_gray
-        diff_abs = np.abs(diff).astype(np.float64)
+        diff_CP = frame_current_gray - frame_prev_gray
+        diff_NC = frame_next_gray - frame_current_gray
+        diff_CP_abs = np.abs(diff_CP).astype(np.float64)
+        diff_NC_abs = np.abs(diff_NC).astype(np.float64)
+
+        diff_abs = (diff_CP_abs + diff_NC_abs) / 2
+        if image_idx == len(input) - 1:
+            diff_abs *= 2
 
         ##### make mask by applying threshold
-        frame_diff = np.where(diff_abs > threshold, 1.0, 0.0)
+        frame_diff_max_thres = np.where(diff_abs > 60, 0.1, -0.2)
+        # frame_diff_mid_thres = np.where(diff_abs > 35, 0.1, -0.2)
+        frame_diff_min_thres = np.where(diff_abs > 15, 0.1, -0.2)
+        frame_diff = frame_diff_max_thres  + frame_diff_min_thres
 
         ##### apply mask to current frame
         current_gray_masked = np.multiply(frame_current_gray, frame_diff)
@@ -94,7 +109,7 @@ def main():
 
         ##### final result
         result = current_gray_masked_mk2.astype(np.uint8)
-        # cv.imshow('result', result)
+        cv.imshow('result', result)
 
         ##### renew background
         frame_prev_gray = frame_current_gray
@@ -106,10 +121,21 @@ def main():
         ##### end of input
         if image_idx == len(input) - 1:
             break
+        if image_idx == len(input) - 2:
+            frame_current = cv.imread(os.path.join(input_path, input[image_idx + 1]))
+            frame_current_gray = cv.cvtColor(frame_current, cv.COLOR_BGR2GRAY).astype(np.float64)
+            frame_next_gray = frame_current_gray
+            continue
 
+            
         ##### read next frame
         frame_current = cv.imread(os.path.join(input_path, input[image_idx + 1]))
         frame_current_gray = cv.cvtColor(frame_current, cv.COLOR_BGR2GRAY).astype(np.float64)
+
+        frame_next = cv.imread(os.path.join(input_path, input[image_idx + 2]))
+        frame_next_gray = cv.cvtColor(frame_current, cv.COLOR_BGR2GRAY).astype(np.float64)
+
+        
 
         ##### If you want to stop, press ESC key
         k = cv.waitKey(30) & 0xff
@@ -118,6 +144,61 @@ def main():
 
     ##### evaluation result
     eval.cal_result(gt_path, result_path)
+
+
+
+
+    
+    
+    #*********************************************************************#
+    # frame_current = cv.imread(os.path.join(input_path, input[0]))
+    # frame_current_gray = cv.cvtColor(frame_current, cv.COLOR_BGR2GRAY).astype(np.float64)
+
+    # ##### background substraction
+    # for image_idx in range(len(input)):
+
+    #     ##### calculate foreground region
+    #     diff = frame_current_gray - frame_prev_gray
+    #     diff_abs = np.abs(diff).astype(np.float64)
+
+    #     ##### make mask by applying threshold
+    #     # frame_diff = np.where(diff_abs > threshold, 1.0, 0.0)
+
+    #     frame_diff_max_thres = np.where(diff_abs > 70, 0.1, -0.2)
+    #     frame_diff_mid_thres = np.where(diff_abs > 37, 0.1, -0.2)
+    #     frame_diff_min_thres = np.where(diff_abs > 5, 0.1, -0.2)
+    #     frame_diff = avg_frame_diff_max_thres + frame_diff_mid_thres + avg_frame_diff_min_thres
+
+    #     ##### apply mask to current frame
+    #     current_gray_masked = np.multiply(frame_current_gray, frame_diff)
+    #     current_gray_masked_mk2 = np.where(current_gray_masked > 0, 255.0, 0.0)
+
+    #     ##### final result
+    #     result = current_gray_masked_mk2.astype(np.uint8)
+    #     # cv.imshow('result', result)
+
+    #     ##### renew background
+    #     frame_prev_gray = frame_current_gray
+
+    #     ##### make result file
+    #     ##### Please don't modify path
+    #     cv.imwrite(os.path.join(result_path, 'result%06d.png' % (image_idx + 1)), result)
+
+    #     ##### end of input
+    #     if image_idx == len(input) - 1:
+    #         break
+
+    #     ##### read next frame
+    #     frame_current = cv.imread(os.path.join(input_path, input[image_idx + 1]))
+    #     frame_current_gray = cv.cvtColor(frame_current, cv.COLOR_BGR2GRAY).astype(np.float64)
+
+    #     ##### If you want to stop, press ESC key
+    #     k = cv.waitKey(30) & 0xff
+    #     if k == 27:
+    #         break
+
+    # ##### evaluation result
+    # eval.cal_result(gt_path, result_path)
 
 if __name__ == '__main__':
     main()
